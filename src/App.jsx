feef51b502239122a9d99ccd3881016a50e19752
humberto-peres <betinhoperes.filho@gmail.com>
2024-04-28 20:56:14 -0300

    CT-72 Página de não encontrado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import routes from './Routes/routes';
-import NotFound from "./pages/NotFound";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
